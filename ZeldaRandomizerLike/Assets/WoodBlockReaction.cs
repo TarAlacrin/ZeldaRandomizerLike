70e24f7225bdfd5ff024d4b49777c17fb7528b3b
Bevans104 <bryceevans104@gmail.com>
2019-12-21 03:51:11 -0500

    Added wooden blocks and transferrable fire for chain reactions. Added smoke and an additional fire prefab for wider burning and reaction vs. torch or arrow.

---
+++ ZeldaRandomizerLike/Assets/WoodBlockReaction.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodBlockReaction : Reaction
+{
+	public GameObject fireVFX;
+	public GameObject smokeVFX;
+	GameObject spawnedSmokeVFX;
+	GameObject spawnedFireVFX;
+	public GameObject graphics;
+	public float lightTime;
+	public float burnTime;
+
+    public override void Start()
+    {
+
+        base.Start();
+    }
+
+    public override void React(Element.PropertyType inElementType)
+	{		
+		if(inElementType == Element.PropertyType.Fire)
+		{
+			if(active){
+				spawnedSmokeVFX = Instantiate(smokeVFX, transform.position, transform.rotation) as GameObject;
+				StartCoroutine(LightC());
+			}
+		}
+
+		base.React(inElementType);
+	}
+
+	public override void Unreact()
+	{
+		StopCoroutine(LightC());
+		Destroy(spawnedSmokeVFX);	
+		base.Unreact();
+	}
+
+	IEnumerator LightC()
+	{
+		yield return new WaitForSeconds(lightTime);
+		active = false;
+		spawnedFireVFX = Instantiate(fireVFX, transform.position, transform.rotation) as GameObject;
+		Destroy(spawnedSmokeVFX);
+		StartCoroutine(BurnC());
+	}
+
+	IEnumerator BurnC()
+	{
+		yield return new WaitForSeconds(burnTime);
+		Destroy(spawnedFireVFX);
+		Destroy(gameObject);
+	}
+}
